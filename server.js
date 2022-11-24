222481578b776d0ae2bdbb8e8032d8f9355fdce4
Luis Carlos Mencia <luiscarlos@lcmencia.com>
2022-11-24 00:23:14 -0300

    handdle erros

--- server.js
+++ server.js
@@ -7,23 +7,40 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.post("/cars", async (req, res) => {
-    const results = await db.createCar(req.body);
-    res.status(201).json({id: results[0]});
+    try{
+        const results = await db.createCar(req.body);
+        res.status(201).json({id: results[0]});
+    }catch(err){
+        res.status(400).json(err);
+    }
+    
 });
 
 app.get("/cars", async (req, res) => {
-    const cars = await db.getAllCars();
-    res.status(200).json({ cars });
+    try{
+        const cars = await db.getAllCars();
+        res.status(200).json({ cars });
+    }catch(err){
+        res.status(400).json(err);
+    }
 });
 
 app.patch("/cars/:id", async (req, res) => {
-    const id = await db.updateCar(req.params.id, req.body);
-    res.status(200).json({ id });
+    try{
+        const id = await db.updateCar(req.params.id, req.body);
+        res.status(200).json({ id });
+    }catch(err){
+        res.status(400).json(err);
+    }
 });
 
-app.delete("/cars/:id", async (req, res) => {
-    await db.deleteCar(req.params.id);
-    res.status(200).json({success:true});
+app.delete("/cars/:id", async (req, res) => { 
+    try{
+        await db.deleteCar(req.params.id);
+        res.status(200).json({success:true});
+    }catch(err){
+        res.status(400).json(err);
+    }
 });
 
 app.listen(1337, () => console.log("server is running on port 1337"));
